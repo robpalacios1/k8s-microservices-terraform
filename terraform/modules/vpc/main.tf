736bf89dba08a1254db2a0fa9c52eb36aaa89a88
roberto <robpalacios1@hotmail.com>
2026-07-21 15:55:08 -0500

    Create Resources for VPC module

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -66,3 +66,104 @@ resource "aws_subnet" "private_subnet_2" {
     Name = "dev-private-subnet-2"
   }
 }
+
+# ====================================================================
+# 4. Create Internet Gatway
+# ====================================================================
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+# ====================================================================
+# 5. Create Public Route Table
+# ====================================================================
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+# ====================================================================
+# 6. Create Private Route Table
+# ====================================================================
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "dev-private-rt"
+  }
+}
+
+# ====================================================================
+# 7. Public Route Table Association
+# ====================================================================
+
+resource "aws_route_table_association" "public_rt_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# ====================================================================
+# 8. Private Route Table Association to each private subnet
+# ====================================================================
+
+resource "aws_route_table_association" "private_rt_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# ====================================================================
+# 9. Create Elastic IP for NAT Gateway
+# ====================================================================
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name : "dev-nat-eip"
+  }
+}
+
+# ====================================================================
+# 10. Create NAT Gateway
+# ====================================================================
+
+resource "aws_nat_gateway" "main_gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "dev-nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.main_igw]
+}
