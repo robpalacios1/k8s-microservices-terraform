65d14a6dde87d205757533f95f061b81023e1ad7
roberto <robpalacios1@hotmail.com>
2026-07-21 11:48:14 -0500

    Create Public and Private Subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,3 +1,7 @@
+# ====================================================================
+# 1. Create VPC
+# ====================================================================
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -10,5 +14,55 @@ resource "aws_vpc" "main_vpc" {
   tags = {
     Name        = "dev-vpc"
     Environment = "dev"
+  }
+}
+
+# ====================================================================
+# 2. Create Public Subnets 
+# ====================================================================
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev-public-subnet-2"
+  }
+}
+
+# ====================================================================
+# 3. Create Private Subnets 
+# ====================================================================
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "dev-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "dev-private-subnet-2"
   }
 }
