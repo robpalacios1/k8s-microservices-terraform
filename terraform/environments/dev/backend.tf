d2ec24f5a71c395e116bf7f5ae0b18fe886ab427
roberto <robpalacios1@hotmail.com>
2026-07-22 17:54:36 -0500

    create Terraform Backend S3 lock

---
+++ terraform/environments/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "my-project-tf-state-us-east-1"
+    key    = "devterraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-ha-dev-locks"
+    encrypt = true
+  }
+}
