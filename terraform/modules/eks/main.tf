266cb294d95b94f1fb4a6c55f9ac2da7b4fc523b
roberto <robpalacios1@hotmail.com>
2026-07-22 11:27:47 -0500

    create EKS cluster in EKS Module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -32,3 +32,29 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
     role       = aws_iam_role.eks_cluster_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+# ====================================================================
+# 2. EKS Cluster
+# ====================================================================
+
+resource "aws_eks_cluster" "dev_eks_cluster" {
+    name = "dev-eks-cluster"
+    role_arn = aws_iam_role.eks_cluster_role.arn
+    version = "1.36"
+
+    vpc_config {
+        subnet_ids = [
+            "subnet-0f9526455f3119597",
+            "subnet-0cc71730fabf5a798"
+        ]
+    }
+
+    depends_on = [
+        aws_iam_role_policy_attachment.eks_cluster_policy
+    ]
+
+    tags = {
+        Name        = "dev-eks-cluster"
+        Environment = "dev"
+    }
+}
