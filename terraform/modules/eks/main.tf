a830ed46396bce3df0f19dde0903e323d933324b
roberto <robpalacios1@hotmail.com>
2026-07-22 11:35:17 -0500

    Create role for EKS Node Group in EKS Module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -58,3 +58,44 @@ resource "aws_eks_cluster" "dev_eks_cluster" {
         Environment = "dev"
     }
 }
+
+# ====================================================================
+# 3. IAM Role for EKS Node Group (worker nodes)
+# ====================================================================
+
+resource "aws_iam_role" "eks_node_role" {
+    name = "dev-eks-node-role"
+
+    assume_role_policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ec2.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
+    })
+
+    tags = {
+      Name        = "dev-eks-node-role"
+      Environment = "dev"
+    }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+    role       = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+    role       = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
+    role       = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
